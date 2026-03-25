9c95d1be42f9bf45e6e71badcf9680aa8fe2ca51
Silas Schulz <silas123.ss@gmail.com>
2026-03-25 13:11:50 +0000

    Added disabled button if data is missing

--- app.py
+++ app.py
@@ -677,4 +677,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()   # creates users.db + users table
     init_db()             # creates pflanzenschutz.db
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host="0.0.0.0")
