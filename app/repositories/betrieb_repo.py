b3ebb5563924e5fd823d73accf1acbf05293de1a
silas123.ss@gmail.com <silas123.ss@gmail.com>
2026-05-25 16:08:23 +0200

    Fixed small things

--- app/repositories/betrieb_repo.py
+++ app/repositories/betrieb_repo.py
@@ -20,6 +20,7 @@ def save_betrieb(data):
         betrieb.plz = data["plz"]
         betrieb.ort = data["ort"]
         betrieb.bundesland = data["bundesland"]
+        db.session.commit()
     else:
         betrieb = Betrieb(
             firma=data["firma"],
--- app/version.py
+++ app/version.py
@@ -1,4 +1,4 @@
-APP_VERSION = "0.0.5"
+APP_VERSION = "0.0.7"
 APP_NAME = "PSMSimple"
 GITHUB_OWNER = "schulsi"
 GITHUB_REPO = "PSMSimple"
