33ff47f41bd0175f9e7ac2dad2c8773f5782e4e4
Silas <silas123.ss@gmail.com>
2026-07-31 18:12:33 +0200

    Added renovate bot (#89) and yaml linting
    * Added renovate bot
    
    * Added yaml linting
    
    * Removed document start rule
    
    * FIxed yaml errors
    
    * Fixed last yaml issues

---
+++ .github/renovate-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  platform: "github",
+
+  repositories: [
+    "schulsi/PSMSimple"
+  ],
+
+  onboarding: false,
+  requireConfig: "required",
+
+  gitAuthor: "Renovate Bot <renovate@users.noreply.github.com>"
+};
